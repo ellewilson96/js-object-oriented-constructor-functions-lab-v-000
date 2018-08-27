5a18fbff3b8230113f3fe4b9286fbe5573ee5561
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-27 15:54:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+function User(name, age, hometown) {
+  this.name = name
+  this.age = age
+  this.hometown = hometown
+} 
+*/
+
+function Scooter(year, color, model) {
+  this.year = year
+  this.color = color
+  this.hometown = hometown
+}
